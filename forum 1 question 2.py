4c1c706b45bb9656f2cdae9d995acb56ac8806cc
VaniaNatalie <71621406+VaniaNatalie@users.noreply.github.com>
2020-09-23 12:23:39 +0700

    Delete forum 1 question 2.py

--- forum 1 question 2.py
+++
@@ -1,3 +0,0 @@
-print("To find acceleration using Newton's Second Law")
-mass, force = [mass for mass in input("Enter the mass in kg and the force in N, separated by comma: ").split(",")]
-print("The acceleration is ", float(force) / float(mass))
