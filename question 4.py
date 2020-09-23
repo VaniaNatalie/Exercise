1baa82bc4d9d56c24c2ec54b16cc1bded6eb9b2e
VaniaNatalie <71621406+VaniaNatalie@users.noreply.github.com>
2020-09-23 12:24:55 +0700

    Delete question 4.py

--- question 4.py
+++
@@ -1,6 +0,0 @@
-pi = "3.14"
-print("The diameter of Jimmy's pie in the question is 55.4 but feel free to input any number:)")
-diameter = input("The diameter of Jimmy's pie is ")
-radius = float(diameter) / 2
-circumference = 2 * float(pi) * float(radius)
-print("Jimmy's pie has a circumference of ", circumference)
