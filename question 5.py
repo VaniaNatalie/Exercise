9793806103b6deae1476fb7ea8df2a458087bd28
VaniaNatalie <71621406+VaniaNatalie@users.noreply.github.com>
2020-09-23 12:25:04 +0700

    Delete question 5.py

--- question 5.py
+++
@@ -1,4 +0,0 @@
-print("To find the wavelength")
-speed = input("The speed (m/s) ")
-frequency = input("The frequency (Hz) ")
-wavelength = print("The wavelngth (m)", float(speed) / float(frequency))
