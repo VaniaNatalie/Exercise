ae9b311fcf51c950a1983b8e94e5f7ba91eb6078
VaniaNatalie <71621406+VaniaNatalie@users.noreply.github.com>
2020-10-09 08:45:38 +0700

    Update question 5.py

--- Discussion Forum Week 4/question 5.py
+++ Discussion Forum Week 4/question 5.py
@@ -1,11 +1,18 @@
 print("Temperature converter")
+def f_to_c(farenheit):
+    celcius = (5 / 9) * (float(farenheit) - 32)
+    return celcius
+
+def c_to_k(celcius):
+    kelvin = celcius + 273
+    return kelvin
+
 def convert_temp():
     farenheit = float(input("Enter a temperature in Farenheit: "))
-    celcius = (5 / 9) * (float(farenheit) - 32)
-    kelvin = celcius + 273
+    celcius = f_to_c(farenheit)
     print(" ")
     print("The temperature in Farenheit is:", farenheit)
-    print("The temperature in Celcius is:", celcius)
-    print("The temperature in Kelvin is:", kelvin)
+    print("The temperature in Celcius is:", f_to_c(farenheit))
+    print("The temperature in Kelvin is:", c_to_k(celcius))
 
 convert_temp()
