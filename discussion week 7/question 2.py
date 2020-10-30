325e7f32b0bbd0c289ea044db3b6ee8a1cafbae4
VaniaNatalie <71621406+VaniaNatalie@users.noreply.github.com>
2020-10-30 08:13:25 +0700

    Update question 2.py

--- discussion week 7/question 2.py
+++ discussion week 7/question 2.py
@@ -1,7 +1,8 @@
 def signup(users):
     new_username = str(input("Enter your new username: "))
     new_password = str(input("Enter your new password: "))
-    return users.setdefault(new_username, new_password)
+    users[new_username] = new_password
+    return users
 
 def login(users, username, password):
     if username in users and password == users[username]:
@@ -10,7 +11,7 @@ def login(users, username, password):
 
 if __name__ == "__main__":
     users = {}
-    print(signup(users))
+    signup(users)
     username = str(input("Enter your username: "))
     password = str(input("Enter your password: "))
     print(login(users,username,password))
